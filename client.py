075fa2c4b9192f65ddc68f1b4407ea136f999dac
bchadwiger <benjamin.hadwiger@gmx.de>
2024-03-18 22:49:22 +0100

    add support for QHY5III485c device and add timestamp to filename

--- client.py
+++ client.py
@@ -1,4 +1,12 @@
+"""
+Implements a simple PyIndi Client.
+
+This code is based on the INDI tutorial at
+https://www.indilib.org/developers/dev-howtos/151-time-lapse-astrophotography-with-indi-python.html
+"""
+
 import sys, time, logging
+import datetime
 import PyIndi
   
 class IndiClient(PyIndi.BaseClient):
@@ -9,12 +17,20 @@ class IndiClient(PyIndi.BaseClient):
         super(IndiClient, self).__init__()
         self.logger = logging.getLogger('PyQtIndi.IndiClient')
         self.logger.info('creating an instance of PyQtIndi.IndiClient')
+        self.timer = None
+    
+    # called when a new device is detected
     def newDevice(self, d):
         self.logger.info("new device " + d.getDeviceName())
         if d.getDeviceName() == "CCD Simulator":
             self.logger.info("Set new device CCD Simulator!")
             # save reference to the device in member variable
             self.device = d
+        elif d.getDeviceName() == "QHY CCD QHY5III485C-792":
+            self.logger.info("Set new device CCD Simulator!")
+            self.device = d
+        else:
+            raise NotImplementedError(f"Device {d.getDeviceName()} not supported!")
     def newProperty(self, p):
         self.logger.info("new property "+ p.getName() + " for device "+ p.getDeviceName())
         if self.device is not None and p.getName() == "CONNECTION" and p.getDeviceName() == self.device.getDeviceName():
@@ -26,8 +42,10 @@ class IndiClient(PyIndi.BaseClient):
         if p.getName() == "CCD_EXPOSURE":
             # take first exposure
             self.takeExposure()
+
     def removeProperty(self, p):
-        self.logger.info("remove property "+ p.getName() + " for device "+ p.getDeviceName())
+        self.logger.info("remove property " + p.getName() + " for device " + p.getDeviceName())
+
     def newBLOB(self, bp):
         self.logger.info("new BLOB "+ bp.name)
         # get image data
@@ -36,44 +54,56 @@ class IndiClient(PyIndi.BaseClient):
         import io
         blobfile = io.BytesIO(img)
         # open a file and save buffer to disk
-        with open("frame.fit", "wb") as f:
+        with open(f"frame_{self.timer}.fit", "wb") as f:
             f.write(blobfile.getvalue())
         # start new exposure
         self.takeExposure()
+
     def newSwitch(self, svp):
-        self.logger.info ("new Switch "+ svp.name + " for device "+ svp.device)
+        self.logger.info ("new Switch " + svp.name + " for device " + svp.device)
+
     def newNumber(self, nvp):
-        self.logger.info("new Number "+ nvp.name + " for device "+ nvp.device)
+        self.logger.info("new Number " + nvp.name + " for device " + nvp.device)
+
     def newText(self, tvp):
-        self.logger.info("new Text "+ tvp.name + " for device "+ tvp.device)
+        self.logger.info("new Text " + tvp.name + " for device " + tvp.device)
+
     def newLight(self, lvp):
-        self.logger.info("new Light "+ lvp.name + " for device "+ lvp.device)
+        self.logger.info("new Light " + lvp.name + " for device " + lvp.device)
+
     def newMessage(self, d, m):
         #self.logger.info("new Message "+ d.messageQueue(m))
         pass
+
     def serverConnected(self):
-        print("Server connected ("+self.getHost()+":"+str(self.getPort())+")")
+        print("Server connected (" + self.getHost() + ":" + str(self.getPort()) + ")")
+
     def serverDisconnected(self, code):
-        self.logger.info("Server disconnected (exit code = "+str(code)+","+str(self.getHost())+":"+str(self.getPort())+")")
+        self.logger.info("Server disconnected (exit code = " + str(code) + "," + str(self.getHost()) + ":" + str(self.getPort()) + ")")
+
     def takeExposure(self):
         self.logger.info(">>>>>>>>")
         #get current exposure time
         exp = self.device.getNumber("CCD_EXPOSURE")
-        # set exposure time to 5 seconds
-        exp[0].value = 5
+        # set exposure time in seconds
+        exp[0].value = 0.01
         # send new exposure time to server/device
+        now = datetime.datetime.now()
+        self.timer = f"{now.year}{now.month:02}{now.day:02}_{now.hour:02}{now.minute:02}{now.second:02}.{int(now.microsecond/1000):03}"
         self.sendNewNumber(exp)
-  
+
+
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
  
 # instantiate the client
-indiclient=IndiClient()
+indiclient = IndiClient()
 # set indi server localhost and port 7624
 indiclient.setServer("localhost",7624)
+
 # connect to indi server
 print("Connecting to indiserver")
-if (not(indiclient.connectServer())):
-     print("No indiserver running on "+indiclient.getHost()+":"+str(indiclient.getPort())+" - Try to run")
+if not indiclient.connectServer():
+     print("No indiserver running on " + indiclient.getHost() + ":" + str(indiclient.getPort()) + " - Try to run")
      print("  indiserver indi_simulator_telescope indi_simulator_ccd")
      sys.exit(1)
   
