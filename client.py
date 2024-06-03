5366358776a8d60d9817955398ce61bfc1cd8d63
bchadwiger <benjamin.hadwiger@gmx.de>
2024-06-03 18:01:11 +0100

    continue implementation of client

--- client.py
+++ client.py
@@ -5,17 +5,23 @@ This code is based on the INDI tutorial at
 https://www.indilib.org/developers/dev-howtos/151-time-lapse-astrophotography-with-indi-python.html
 """
 
+from astropy.io import fits
+import numpy as np
+import cv2
+import os
+import io
 import sys, time, logging
 import argparse
 import datetime
 import PyIndi
 import warnings
- 
+
+
 class IndiClient(PyIndi.BaseClient):
  
     device = None
  
-    def __init__(self, exposure, x0, y0, width, height):
+    def __init__(self, exposure, x0, y0, width, height, raw, override):
         super(IndiClient, self).__init__()
         self.logger = logging.getLogger('PyQtIndi.IndiClient')
         self.logger.info('creating an instance of PyQtIndi.IndiClient')
@@ -30,7 +36,9 @@ class IndiClient(PyIndi.BaseClient):
         self.y0 = y0
         self.width = width
         self.height = height
-    
+        self.raw = raw
+        self.override = override
+
     # called when a new device is detected
     def newDevice(self, d):
         self.logger.info("new device " + d.getDeviceName())
@@ -78,14 +86,30 @@ class IndiClient(PyIndi.BaseClient):
     def newBLOB(self, bp):
         self.logger.info("new BLOB "+ bp.name)
         # get image data
-        img = bp.getblobdata()
-        # write image data to BytesIO buffer
-        import io
-        blobfile = io.BytesIO(img)
-        # open a file and save buffer to disk
-        # with open(f"frame_{self.timer}_exp_{self.exposure}s.fit", "wb") as f:
-        with open(f"frame.fit", "wb") as f:
-            f.write(blobfile.getvalue())
+        blobdata = bp.getblobdata()
+        blobfile = io.BytesIO(blobdata)
+
+        if self.override:
+            out_basename = 'frame'
+        else:
+            out_basename = f"frame_{self.timer}_exp_{self.exposure}s"
+
+        if self.raw:
+            out_filename = out_basename + '.fit'
+            with open(out_filename, 'wb') as f:
+                f.write(blobfile.getvalue())
+        else:
+            out_filename = out_basename + '.png'
+            print("\n\n\nBEFORE\n\n\n")
+            img_fits = fits.getdata(blobfile) * ((2**8 - 1) / (2**16 - 1))
+            print("\n\n\nAFTER\n\n\n")
+            img_fits = img_fits.astype('uint8')
+
+            # RGGB (used by QHY5III485) corresponds to OpenCV BR Bayer pattern,
+            # see https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html
+            imgBGR = cv2.cvtColor(img_fits, cv2.COLOR_BAYER_BG2BGR)
+            cv2.imwrite(out_filename, imgBGR)
+
         # start new exposure
         self.takeExposure()
 
@@ -112,6 +136,8 @@ class IndiClient(PyIndi.BaseClient):
         self.logger.info("Server disconnected (exit code = " + str(code) + "," + str(self.getHost()) + ":" + str(self.getPort()) + ")")
 
     def takeExposure(self):
+
+        # TODO: adapt exposure based on stats of previous image(s)
         self.logger.info(">>>>>>>>")
         
         #get current exposure time
@@ -130,17 +156,25 @@ if __name__ == '__main__':
                       prog='Client.py',
                       description='Cyclicly records images with given parameters')
     parser.add_argument('exposure', type=float, help='Exposure in seconds')
-    parser.add_argument('-x0', type=int, default=856, help='Left-most pixel position') # theoretically 842 for center square)
+    parser.add_argument('-x0', type=int, default=880, help='Left-most pixel position') # theoretically 842 for center square)
     parser.add_argument('-y0', type=int, default=0, help='Top-most pixel position')
     parser.add_argument('-W', '--width', type=int, default=2180, help='Frame width in pixels')
     parser.add_argument('-H', '--height', type=int, default=2180, help='Frame height in pixels')
+    parser.add_argument('-r', '--raw', type=bool, default=False, help='Whether to store raw image (as fits), or debayered image (as png) (default)', action=argparse.BooleanOptionalAction)
+    parser.add_argument('-w', '--override', type=bool, default=False, help='Whether to use same filename for each new image (and thus override image with each new exposure). Otherwise, uses a unique filename based on timestamp, etc. (default).', action=argparse.BooleanOptionalAction)
+    parser.add_argument('-o', '--output_dir', type=str, default=f'{os.getcwd()}/images', action='store')
     args = parser.parse_args()
     print(f'Using the following parameters: {args}')
+
+    image_dir = args.output_dir
+    print(f'Using output directory: {image_dir}')
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
 
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
     # instantiate the client
-    indiclient = IndiClient(args.exposure, args.x0, args.y0, args.width, args.height)
+    indiclient = IndiClient(args.exposure, args.x0, args.y0, args.width, args.height, args.raw, args.override)
     # set indi server localhost and port 7624
     indiclient.setServer("localhost",7624)
 
